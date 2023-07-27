45d6674c12ec7e42b52a643c04fbcef293b84f60
tkn13 <thaksin.tkn@gmail.com>
2023-07-27 22:17:21 +0700

    week1-v0.1

---
+++ week1/HelloWorld.java
@@ -0,0 +1,6 @@
+/* public class [className]  => [className] have to match with file name */
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
